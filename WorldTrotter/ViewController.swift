32754efe8aeaa2e80a40d9efac8a62b1aae0ff72
miPera <migperalta10@gmail.com>
2017-08-12 19:24:53 -0700

    Changed secondView to be a subview of firstView

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -13,6 +13,6 @@ class ViewController: UIViewController {
         let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
         let secondView = UIView(frame: secondFrame)
         secondView.backgroundColor = UIColor.green
-        view.addSubview(secondView)
+        firstView.addSubview(secondView)
     }
 }
