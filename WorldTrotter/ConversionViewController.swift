998b029e3fe4b8964bd1345a2ed17de01737017b
miPera <migperalta10@gmail.com>
2017-08-22 19:16:58 -0700

    Celsius label display ??? as opposed to no value when the farheinheit field is empty

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -4,6 +4,12 @@ class ConversionViewController: UIViewController {
     @IBOutlet var  celsiusLabel: UILabel!
     
     @IBAction func fahrenheightFieldEditingChanged(_ textField: UITextField) {
-        celsiusLabel.text = textField.text
+        
+        //text field has text and text is not empty
+        if let text = textField.text, !text.isEmpty {
+            celsiusLabel.text = text
+        } else {
+            celsiusLabel.text = "???"
+        }
     }
 }
