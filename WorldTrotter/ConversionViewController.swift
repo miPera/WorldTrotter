6712a9989271faaa7941362d8571b5dcfdd44ac0
miPera <migperalta10@gmail.com>
2017-08-22 19:14:23 -0700

    Added connections between conversion view controller and interface builder

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -1,5 +1,9 @@
 import UIKit
 
 class ConversionViewController: UIViewController {
+    @IBOutlet var  celsiusLabel: UILabel!
     
+    @IBAction func fahrenheightFieldEditingChanged(_ textField: UITextField) {
+        celsiusLabel.text = textField.text
+    }
 }
